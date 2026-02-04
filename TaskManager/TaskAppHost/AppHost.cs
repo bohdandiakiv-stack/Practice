083480ecefeeb7c4c961242bd35deb2f1e8e210e
pmovsyan <pmovsyan@msp.amadeus.net>
2026-02-04 11:06:47 +0000

    The TaskAppHost proj has been updated on Aspire 13.1.0

--- TaskManager/TaskAppHost/AppHost.cs
+++ TaskManager/TaskAppHost/AppHost.cs
@@ -1,61 +1,108 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Aspire.Hosting;
+using Couchbase.Aspire.Hosting;
+using Microsoft.Extensions.Configuration;
+using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
-// Couchbase config
-var couchbaseSection = config.GetSection("Couchbase");
-var cbImage = couchbaseSection.GetValue<string>("Image");
-var cbTag = couchbaseSection.GetValue<string>("Tag");
-var cbAdminUser = couchbaseSection.GetValue<string>("AdminUser");
-var cbAdminPassword = couchbaseSection.GetValue<string>("AdminPassword");
-var cbDataVolume = couchbaseSection.GetValue<string>("DataVolume");
-var cbDataPath = couchbaseSection.GetValue<string>("DataPath");
-var cbPorts = couchbaseSection.GetSection("Ports");
-var cbConsolePort = cbPorts.GetValue<int>("Console");
-var cbMemcachedPort = cbPorts.GetValue<int>("Memcached");
-var cbQueryPort = cbPorts.GetValue<int>("Query");
+IResourceBuilder<CouchbaseClusterResource> couchbasedb = null;
+#region Legacy
+//// Couchbase config
+//var couchbaseSection = config.GetSection("Couchbase");
+//var cbImage = couchbaseSection.GetValue<string>("Image");
+//var cbTag = couchbaseSection.GetValue<string>("Tag");
+//var cbAdminUser = couchbaseSection.GetValue<string>("AdminUser");
+//var cbAdminPassword = couchbaseSection.GetValue<string>("AdminPassword");
+//var cbDataVolume = couchbaseSection.GetValue<string>("DataVolume");
+//var cbDataPath = couchbaseSection.GetValue<string>("DataPath");
+//var cbPorts = couchbaseSection.GetSection("Ports");
+//var cbConsolePort = cbPorts.GetValue<int>("Console");
+//var cbMemcachedPort = cbPorts.GetValue<int>("Memcached");
+//var cbQueryPort = cbPorts.GetValue<int>("Query");
 
-// Couchbase Init config
-var cbInitSection = config.GetSection("CouchbaseInit");
-var cbInitImage = cbInitSection.GetValue<string>("Image");
-var cbInitScriptHostPath = cbInitSection.GetValue<string>("ScriptHostPath");
-var cbInitScriptContainerPath = cbInitSection.GetValue<string>("ScriptContainerPath");
+//// Couchbase Init config
+//var cbInitSection = config.GetSection("CouchbaseInit");
+//var cbInitImage = cbInitSection.GetValue<string>("Image");
+//var cbInitScriptHostPath = cbInitSection.GetValue<string>("ScriptHostPath");
+//var cbInitScriptContainerPath = cbInitSection.GetValue<string>("ScriptContainerPath");
 
-// API config
-var apiSection = config.GetSection("TaskManagerApi");
-var apiDockerfilePath = apiSection.GetValue<string>("DockerfilePath");
-var apiDockerfileName = apiSection.GetValue<string>("DockerfileName");
-var apiPort = apiSection.GetValue<int>("Port");
-var apiTargetPort = apiSection.GetValue<int>("TargetPort");
-var apiConnectionString = apiSection.GetSection("ConnectionStrings").GetValue<string>("Couchbase");
-var apiCbSection = apiSection.GetSection("Couchbase");
-var apiCbUsername = apiCbSection.GetValue<string>("Username");
-var apiCbPassword = apiCbSection.GetValue<string>("Password");
-var apiCbBucketName = apiCbSection.GetValue<string>("BucketName");
+//// API config
+//var apiSection = config.GetSection("TaskManagerApi");
+//var apiDockerfilePath = apiSection.GetValue<string>("DockerfilePath");
+//var apiDockerfileName = apiSection.GetValue<string>("DockerfileName");
+//var apiPort = apiSection.GetValue<int>("Port");
+//var apiTargetPort = apiSection.GetValue<int>("TargetPort");
+//var apiConnectionString = apiSection.GetSection("ConnectionStrings").GetValue<string>("Couchbase");
+//var apiCbSection = apiSection.GetSection("Couchbase");
+//var apiCbUsername = apiCbSection.GetValue<string>("Username");
+//var apiCbPassword = apiCbSection.GetValue<string>("Password");
+//var apiCbBucketName = apiCbSection.GetValue<string>("BucketName");
 
-// Couchbase container
-var couchbase = builder.AddContainer("couchbase", cbImage, cbTag)
-    .WithHttpEndpoint(port: cbConsolePort, targetPort: cbConsolePort, name: "console")
-    .WithEndpoint(port: cbMemcachedPort, targetPort: cbMemcachedPort, name: "memcached")
-    .WithEndpoint(port: cbQueryPort, targetPort: cbQueryPort, name: "query")
-    .WithEnvironment("COUCHBASE_ADMINISTRATOR_USERNAME", cbAdminUser)
-    .WithEnvironment("COUCHBASE_ADMINISTRATOR_PASSWORD", cbAdminPassword)
-    .WithVolume(cbDataVolume, cbDataPath);
+//// Couchbase container
+//var couchbase = builder.AddContainer("couchbase", cbImage, cbTag)
+//    .WithHttpEndpoint(port: cbConsolePort, targetPort: cbConsolePort, name: "console")
+//    .WithEndpoint(port: cbMemcachedPort, targetPort: cbMemcachedPort, name: "memcached")
+//    .WithEndpoint(port: cbQueryPort, targetPort: cbQueryPort, name: "query")
+//    .WithEnvironment("COUCHBASE_ADMINISTRATOR_USERNAME", cbAdminUser)
+//    .WithEnvironment("COUCHBASE_ADMINISTRATOR_PASSWORD", cbAdminPassword)
+//    .WithVolume(cbDataVolume, cbDataPath);
 
-// Couchbase init container
-var couchbaseInit = builder.AddContainer("couchbase-init", cbInitImage)
-    .WithBindMount(cbInitScriptHostPath, cbInitScriptContainerPath)
-    .WithArgs("sh", cbInitScriptContainerPath)
-    .WaitFor(couchbase);
+//// Couchbase init container
+//var couchbaseInit = builder.AddContainer("couchbase-init", cbInitImage)
+//    .WithBindMount(cbInitScriptHostPath, cbInitScriptContainerPath)
+//    .WithArgs("sh", cbInitScriptContainerPath)
+//    .WaitFor(couchbase);
 
-// API container
-var api = builder.AddDockerfile("taskmanager-api", apiDockerfilePath, apiDockerfileName)
-    .WithHttpEndpoint(port: apiPort, targetPort: apiTargetPort, name: "api-http")
-    .WithEnvironment("ConnectionStrings__couchbase", apiConnectionString)
-    .WithEnvironment("Couchbase__Username", apiCbUsername)
-    .WithEnvironment("Couchbase__Password", apiCbPassword)
-    .WithEnvironment("Couchbase__BucketName", apiCbBucketName)
-    .WaitFor(couchbaseInit);
+//// API container
+//var api = builder.AddDockerfile("taskmanager-api", apiDockerfilePath, apiDockerfileName)
+//    .WithHttpEndpoint(port: apiPort, targetPort: apiTargetPort, name: "api-http")
+//    .WithEnvironment("ConnectionStrings__couchbase", apiConnectionString)
+//    .WithEnvironment("Couchbase__Username", apiCbUsername)
+//    .WithEnvironment("Couchbase__Password", apiCbPassword)
+//    .WithEnvironment("Couchbase__BucketName", apiCbBucketName)
+//    .WaitFor(couchbaseInit);
+#endregion
+
+#region Couchbase Setup  
+
+couchbasedb = builder
+    .AddCouchbase("couchbase")
+    .WithManagementPort(8091);
+
+
+var bucket = couchbasedb.AddBucket("tasks");
+
+
+couchbasedb.ApplicationBuilder.Eventing.Subscribe<ConnectionStringAvailableEvent>(
+    couchbasedb.Resource,
+    async (@event, ct) =>
+    {
+        try
+        {
+            var connectionString = await couchbasedb
+                        .Resource.ConnectionStringExpression.GetValueAsync(ct)
+                        .ConfigureAwait(false);
+            Console.WriteLine($"Connecting to Couchbase with connection string: {connectionString}");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error obtaining Couchbase connection string: {ex.Message}");
+        }
+    }
+);
+
+
+#endregion
+
+ 
+var taskApiProject = builder.AddProject<Projects.TaskManager_Api>("tasks-application-api")
+       .WithUrlForEndpoint("http", url =>
+       {
+           url.DisplayText = "Swagger";
+           url.Url = "/swagger";
+       });
+taskApiProject.WithReference(couchbasedb).WaitFor(couchbasedb);
 
 builder.Build().Run();
--- TaskManager/TaskManager.Infrastructure/Repositories/TaskRepository.cs
+++ TaskManager/TaskManager.Infrastructure/Repositories/TaskRepository.cs
@@ -1,58 +1,55 @@
 ﻿using Couchbase;
 using Couchbase.KeyValue;
 
-namespace TaskManager.Infrastructure.Repositories
+namespace TaskManager.Infrastructure.Repositories;
+
+public class TaskRepository : ITaskRepository
 {
-    public class TaskRepository : ITaskRepository
+
+    private readonly IBucket _bucket;
+    private readonly ICouchbaseCollection _collection;
+
+    public TaskRepository(IBucket bucket, IScope scope, ICouchbaseCollection collection)
     {
+        _bucket = bucket;
+        _collection = collection;
+    }
 
-        private readonly IBucket _bucket;
-        private readonly IScope _scope;
-        private readonly ICouchbaseCollection _collection;
+    public async Task<TaskItem?> GetByIdAsync(string id, CancellationToken cancellationToken = default)
+    {
+        var result = await _collection.GetAsync(id);
+        return result.ContentAs<TaskItem>();
+    }
 
-        public TaskRepository(IBucket bucket, IScope scope, ICouchbaseCollection collection)
+    public async Task<IEnumerable<TaskItem>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        var cluster = _bucket.Cluster;
+        var query = "SELECT t.* FROM `TaskManager` t";
+
+        var result = await cluster.QueryAsync<TaskItem>(query);
+        var items = new List<TaskItem>();
+        await foreach (var item in result.Rows.WithCancellation(cancellationToken))
         {
-            _bucket = bucket;
-            _scope = scope;
-            _collection = collection;
+            items.Add(item);
         }
+        return items;
+    }
 
-        public async Task<TaskItem?> GetByIdAsync(string id, CancellationToken cancellationToken = default)
-        {
-            var result = await _collection.GetAsync(id);
-            return result.ContentAs<TaskItem>();
-        }
+    public async Task<TaskItem> CreateAsync(TaskItem task, CancellationToken cancellationToken = default)
+    {
+        await _collection.InsertAsync(task.Id, task);
+        return task;
+    }
 
-        public async Task<IEnumerable<TaskItem>> GetAllAsync(CancellationToken cancellationToken = default)
-        {
-            var cluster = _bucket.Cluster;
-            var query = "SELECT t.* FROM `TaskManager` t";
+    public async Task<TaskItem> UpdateAsync(TaskItem task, CancellationToken cancellationToken = default)
+    {
+        await _collection.ReplaceAsync(task.Id, task);
+        return task;
+    }
 
-            var result = await cluster.QueryAsync<TaskItem>(query);
-            var items = new List<TaskItem>();
-            await foreach (var item in result.Rows.WithCancellation(cancellationToken))
-            {
-                items.Add(item);
-            }
-            return items;
-        }
-
-        public async Task<TaskItem> CreateAsync(TaskItem task, CancellationToken cancellationToken = default)
-        {
-            await _collection.InsertAsync(task.Id, task);
-            return task;
-        }
-
-        public async Task<TaskItem> UpdateAsync(TaskItem task, CancellationToken cancellationToken = default)
-        {
-            await _collection.ReplaceAsync(task.Id, task);
-            return task;
-        }
-
-        public async Task<bool> DeleteAsync(string id, CancellationToken cancellationToken = default)
-        {
-            await _collection.RemoveAsync(id);
-            return true;
-        }
+    public async Task<bool> DeleteAsync(string id, CancellationToken cancellationToken = default)
+    {
+        await _collection.RemoveAsync(id);
+        return true;
     }
 }
